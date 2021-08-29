9c034b7f9d1f4daecc0cdd1314c56e4bb930e1d4
soham.c <soham.c@highradius.com>
2021-08-29 15:31:41 +0530

    adding files

---
+++ src/main/java/sonarqubetest/sonarqubetest/App.java
@@ -0,0 +1,13 @@
+package sonarqubetest.sonarqubetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
