2d7d63f0720772d0d338d7947cba1e83b5a4f569
sam-soham <89328881+sam-soham@users.noreply.github.com>
2021-08-30 08:30:45 +0530

    Update App.java

--- src/main/java/sonarqubetest/sonarqubetest/App.java
+++ src/main/java/sonarqubetest/sonarqubetest/App.java
@@ -1,4 +1,9 @@
 package sonarqubetest.sonarqubetest;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.*;
+  
 
 /**
  * Hello world!
@@ -6,10 +11,10 @@ package sonarqubetest.sonarqubetest;
  */
 public class App 
 {
+    private final static Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static void main( String[] args )
     {
-      System.out.println("Hello");   
-      System.out.println("Hello");   
-      System.out.println("Hello");   
+      for(int i=0; i<10; i++){
+          log.log(Level.INFO, "My first Log Message");
     }
 }
